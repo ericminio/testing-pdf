c9ce1068071291c3aceedba1ae6dfa6297e73ea6
Eric Mignot <eric.mignot@tecsys.com>
2016-06-28 13:49:05 -0400

    more focused test

--- src/test/java/LovePdfFileTest.java
+++ src/test/java/LovePdfFileTest.java
@@ -1,40 +1,31 @@
 import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.text.PDFTextStripper;
 import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 
 import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
 public class LovePdfFileTest {
 
     @Test
     public void exists() {
-        assertThat(file().exists(), equalTo(true));
+        assertThat(resource(), notNullValue());
     }
 
-    private File file() {
-        return new File(getClass().getClassLoader().getResource("love.pdf").getFile());
-    }
-
-    @Test
-    public void hasContentOnFirstPage() throws IOException {
-        PDDocument document = PDDocument.load(file());
-        PDPage page = document.getDocumentCatalog().getPages().get(0);
-
-        assertThat(page.hasContents(), equalTo(true));
+    private URL resource() {
+        return getClass().getClassLoader().getResource("love.pdf");
     }
 
     @Test
     public void hasExpectedContent() throws IOException {
-        PDDocument document = PDDocument.load(file());
         PDFTextStripper pdfStripper = new PDFTextStripper();
-        pdfStripper.setStartPage(1);
-        pdfStripper.setEndPage(1);
+        String fileName = resource().getFile();
+        PDDocument document = PDDocument.load(new File(fileName));
         String content = pdfStripper.getText(document);
 
         assertThat(content, containsString("Love is everywhere"));
